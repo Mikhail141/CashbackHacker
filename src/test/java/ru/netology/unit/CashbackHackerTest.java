ca6f0c1370f2253783a12d1e23df96647d191962
Mikhail141 <70147484+Mikhail141@users.noreply.github.com>
2020-11-18 11:51:46 +0300

    add name

--- src/test/java/ru/netology/unit/CashbackHackerTest.java
+++ src/test/java/ru/netology/unit/CashbackHackerTest.java
@@ -2,13 +2,11 @@ package ru.netology.unit;
 
 import org.testng.annotations.Test;
 
-import static org.testng.Assert.*;
-
 public class CashbackHackerTest {
 
     @Test
     public void shouldReturnifAmount1500() {
-    CashbackHacker cashbackHacker = new CashbackHacker();
+    CashbackHackerService cashbackHacker = new CashbackHackerService();
 
 }
 }
