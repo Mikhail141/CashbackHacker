ae1d50988c6761057b8135ca3b0fbacab0d3a104
Mikhail141 <70147484+Mikhail141@users.noreply.github.com>
2020-11-19 17:16:45 +0300

    change

--- src/test/java/ru/netology/unit/CashbackHackerServiceTest.java
+++ src/test/java/ru/netology/unit/CashbackHackerServiceTest.java
@@ -12,21 +12,21 @@ public class CashbackHackerServiceTest {
     public void shouldAmount1000() {
         int actual = cashback.remain(1000);
         int expected = 0;
-        assertEquals(actual, expected);
+        assertEquals(expected,actual);
     }
 
     @Test
     public void shouldAmountLess1000() {
         int actual = cashback.remain(900);
         int expected = 100;
-        assertEquals (actual, expected);
+        assertEquals (expected,actual);
     }
 
     @Test
     public void shouldAmountZero() {
         int actual = cashback.remain(0);
         int expected = 1000;
-        assertEquals(actual, expected);
+        assertEquals(expected,actual);
     }
 }
 
