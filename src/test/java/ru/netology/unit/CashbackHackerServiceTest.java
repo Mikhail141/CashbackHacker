38eb8fb01fc2087c12a9595e755ac3b270556f97
Mikhail141 <70147484+Mikhail141@users.noreply.github.com>
2020-11-19 10:43:45 +0300

    add junit4-platform

--- src/test/java/ru/netology/unit/CashbackHackerServiceTest.java
+++ src/test/java/ru/netology/unit/CashbackHackerServiceTest.java
@@ -8,25 +8,27 @@ import static org.junit.Assert.*;
 public class CashbackHackerServiceTest {
     CashbackHackerService cashback = new CashbackHackerService();
 
-    @Test
-    public void shouldAmount1000() {
-        int actual = cashback.remain(1000);
-        int expected = 0;
-        assertEquals(actual, expected);
-    }
 
     @Test
-    public void shouldAmountLess1000() {
-        int actual = cashback.remain(900);
-        int expected = 100;
-        assertEquals (actual, expected);
+    public void shouldAmountless1000() {
+
+        int actual = cashback.remain(850);
+        int expected = 150;
+        assertEquals(actual, expected);
     }
 
     @Test
     public void shouldAmountZero() {
+
         int actual = cashback.remain(0);
         int expected = 1000;
         assertEquals(actual, expected);
+    }
+    @Test
+    public void shouldAmount1000(){
+        int actual = cashback.remain(1000);
+        int expected = 0;
+        assertEquals(actual,expected);
     }
 }
 
